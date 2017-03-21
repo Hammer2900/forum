b02cebd1b0cc04f01b6d4863e9d024fdcccd5565
izot <evgeny2900@gmail.com>
2017-03-21 11:40:04 +0200

    default value for forum

--- form.py
+++ form.py
@@ -5,8 +5,8 @@ import web
 def post_add_form(page_id=''):
     return web.form.Form(
         web.form.Textbox("username", web.form.notnull, value='Аноним', size=20, description="Имя"),
-        web.form.Password("password", description="Пароль"),
-        web.form.Textbox("title", web.form.notnull, size=50, description="Заголовок"),
+        web.form.Password("password", value='1', description="Пароль"),
+        web.form.Textbox("title", web.form.notnull, value='***', size=50, description="Заголовок"),
         web.form.Textarea("content", web.form.notnull, rows=10, cols=70, description="Контент"),
         web.form.Hidden('id', value=page_id),
         web.form.Button(u"Опубликовать"),
